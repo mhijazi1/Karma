08174d42ed162cd601243686a6c044ec59521f5a
mhijazi1 <mhijazi1@student.gsu.edu>
2016-01-01 05:07:17 -0500

    Removed extra testing code, removed dissapointed obama as default picture a hid image element until first image is submitted.

--- app/public/javascripts/script.js
+++ app/public/javascripts/script.js
@@ -9,11 +9,11 @@ app.config(function($routeProvider) {
 });
 
 app.controller('mainController', function($scope) {
-	$scope.image = "http://i2.cdn.turner.com/cnnnext/dam/assets/130410081024-obama-serious-face-horizontal-large-gallery.jpg";
-	$scope.url2 = "Abc123";
+	$scope.imageVis = false;
+	$scope.image = "";
 	$scope.prediction = {url: '', result: ''};
 	$scope.post = function(){
-		$scope.url2 = $scope.prediction.url;
 		$scope.image = $scope.prediction.url;
+		$scope.imageVis = true;
 	}
 });
