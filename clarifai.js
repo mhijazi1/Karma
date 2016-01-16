63a2ee29988cb3f8736861e358c4c81c27015c9a
mhijazi1 <mhijazi1@student.gsu.edu>
2016-01-16 13:06:57 -0500

    Added predict and collections to clarfai api

--- clarifai.js
+++ clarifai.js
@@ -1,17 +1,19 @@
+//'use strict';
+
 var util = require('util');
 var request = require('request');
-//Singleton Clarifai Connection. 
+//Singleton Clarifai Connection.
 var Clarifai = function() {};
 
 module.exports = Clarifai;
 
 Clarifai.init = function(options) {
     validateConstructor(options, function(valid) {
-        util.log(valid);
         if (valid === false) {
             return false;
         }
-        util.log("validated");
+        log("15: validated");
+        this.accessToken = "";
         this.collectionCreated = false;
         this.addDocumentQueue = [];
         this.baseUrl = options.baseUrl || 'https://api-alpha.clarifai.com/v1/';
@@ -21,7 +23,7 @@ Clarifai.init = function(options) {
         }
         this.collectionId = options.collectionId || 'default';
         this.nameSpace = options.nameSpace || 'default';
-        getAccessToken(options, listCollections(function(){}));
+        getAccessToken(options, createCollection.bind(this));
         //          .then(
         //               this.createCollection.bind(this),
         //              this.onError.bind(this)
@@ -30,9 +32,52 @@ Clarifai.init = function(options) {
     });
 };
 
+Clarifai.predict = function(url, concept, callback) {
+    var data = {
+        urls: [url]
+    };
+
+    request.post({
+        url: this.baseUrl + 'curator/concepts/' + this.nameSpace + '/' + concept + '/predict',
+        contentType: 'application/json; charset=utf-8',
+        processData: false,
+        data: JSON.stringify(data),
+        headers: {
+            Authorization: 'Bearer ' + this.accessToken
+        }
+    }, function(err, response, body) {
+        if (err) {
+            log("Clarifai: Predict on " + concept + " failed", e);
+            var result = {
+                'success': false
+            }
+            if (callback) {
+                callback.call(this, result);
+            }
+        }
+
+        if (json.status.status === "OK") {
+            log("Clarifai: Predict on " + concept + " success");
+            var result = json.urls[0];
+            result.success = true;
+            if (callback) {
+                callback.call(this, result);
+            }
+        } else if (json.status.status === 'ERROR') {
+            log("Clarifai: Predict on " + concept + " failed", json);
+            var result = {
+                'success': false
+            }
+            if (callback) {
+                callback.call(this, result);
+            }
+        }
+
+    });
+};
 
 // make sure we got what we need in constructor
-validateConstructor = function(options, isValid) {
+var validateConstructor = function(options, isValid) {
     if (!options.clientSecret && !options.clientId) {
         console.error("Please provide a clientId and clientSecret https://developer-alpha.clarifai.com/docs/auth");
         isValid(false);
@@ -45,171 +90,113 @@ validateConstructor = function(options, isValid) {
         console.error("Please provide a clientId https://developer-alpha.clarifai.com/docs/auth");
         isValid(false);
     }
-    util.log("Done!");
+    log("50: Done!");
     isValid(true);
 };
 
 
 // get an accessToken from localStorage or API
-getAccessToken = function(options, callback) {
+var getAccessToken = function(options, callback) {
+    log("57: getAT");
     if (options.clientId && options.clientSecret) {
         fetchAccessToken(options.clientId, options.clientSecret, function(err, response, body) {
             if (err) {
-                console.error("ERROR: "+err.syscall +" - "+ err.code);
+                console.error("ERROR: " + err.syscall + " - " + err.code);
                 return;
             }
             var json = JSON.parse(body);
             var now = new Date().getTime();
             json.expireTime = now + json.expires_in;
             this.accessToken = json.access_token;
-            util.log("AToken Reg: " +json.access_token);
-        });
+            log("AT: " + this.accessToken);
+            log("AToken Reg: " + json.access_token);
+            callback();
+        }.bind(this));
     } else {
         console.error("need a clientId and clientSecret");
         return;
     }
-    //callback;
-}
+};
 
-fetchAccessToken = function(clientId, clientSecret, returnAccessToken) {
+var fetchAccessToken = function(clientId, clientSecret, returnAccessToken) {
+    log("79: Fetch AT");
     var tokenUrl = this.baseUrl + 'token';
 
     var data = {
         grant_type: "client_credentials",
         client_id: clientId,
         client_secret: clientSecret
-    }
-    util.log(tokenUrl);
-    util.log(data);
+    };
+    //    log(tokenUrl);
+    //    log(data);
     request.post({
         url: tokenUrl,
         form: data
     }, returnAccessToken);
+};
+
+var createCollection = function() {
+    log("96: Create Collection");
+    listCollections(function(collectionCreated) {
+        if (collectionCreated === true) {
+            var result = {
+                success: true
+            };
+        } else {
+            var data = {
+                collection: {
+                    id: this.collectionId,
+                    settings: {
+                        max_num_docs: 100000
+                    }
+                }
+            };
+            log("Data: " + data);
+            request.post({
+                url: this.baseUrl + 'curator/collections',
+                data: JSON.stringify(data),
+                processData: false,
+                contentType: 'application/json; charset=utf-8',
+                headers: {
+                    Authorization: 'Bearer ' + this.accessToken
+                }
+            }, function(err, response, body) {
+                log("err2: " + err + "\n response: " + response + "\n body: " + body);
+            });
+        }
+    });
+};
+
+var listCollections = function(callback) {
+    var collectionCreated = false;
+    log("129: List Collections");
+    log("Access Token: " + this.accessToken);
+    request.get({
+        'url': this.baseUrl + 'curator/collections',
+        'headers': {
+            'Authorization': 'Bearer ' + this.accessToken
+        }
+    }, function(err, response, body) {
+        if (err) {
+            return;
+        }
+
+        if (body.status === 'OK') {
+            for (var i = 0; i < json.collections.length; i++) {
+                var collectionId = json.collections[i].id;
+                if (collectionId === this.collectionId) {
+                    this.collectionCreated = true;
+                    break;
+                }
+            }
+        }
+        return callback(collectionCreated);
+    });
 }
 
-/*createCollection = function(){
-    var deferred = $.Deferred();
-    this.listCollections().then(
-        function(collectionCreated){
-            if(collectionCreated === true){
-                var result = {
-                    'success': true
-                }
-                deferred.resolve(result);   
-            }
-            else{
-                var data = {
-                    'collection': {
-                        'id': this.collectionId,
-                        'settings': {
-                            'max_num_docs': 100000
-                        }
-                    }
-                }
-                $.ajax(
-                    {
-                        'type': 'POST',
-                        'url': this.baseUrl + 'curator/collections',
-                        'data': JSON.stringify(data),
-                        'processData': false,
-                        'contentType': 'application/json; charset=utf-8',
-                        'headers': {
-                            'Authorization': 'Bearer ' + this.accessToken
-                        }
-                    }  
-                ).then(
-                    function(json){
-                        if(json.status.status === "OK"){
-                            this.log("Clarifai: Collection: '" + this.collectionId + "' created");
-                            this.collectionCreated = true;
-                            var result = {
-                                'success': true
-                            }
-                            deferred.resolve(result);
-                        }
-                        if(json.status.status === 'ERROR'){
-                            if(json.status.message.indexOf('Bad request: Collection "' + this.collectionId + '" already exists for user') !== -1){
-                                this.collectionCreated = true;
-                                var result = {
-                                    'success': true
-                                }
-                                deferred.resolve(result);
-                            }
-                            else{
-                                console.error("Clarifai: Error instantiating Clarifai object", json);
-                                var result = {
-                                    'success': false
-                                }
-                                deferred.resolve(result);
-                            }
-                        }
-                    }.bind(this),
-                    function(e){
-                        if(e.status === 409){
-                            var result = {
-                                'success': true
-                            }
-                            deferred.resolve(result);
-                        }
-                        else{
-                            console.error("Clarifai: Error instantiating Clarifai object", e);
-                            console.error(e.responseJSON.status_msg);
-                            if(e.responseJSON.status_msg === 'Token is not valid. Please use valid tokens for a application in your account.'){
-                                console.info("Please make sure you are using a valid accessToken https://developer-alpha.clarifai.com/docs/auth");
-                            }
-                            var result = {
-                                'success': false
-                            }
-                            deferred.resolve(result);
-                        }
-                    }.bind(this)
-                );
-            }
-        }.bind(this),
-        function(e){
-            deferred.reject(e);        
-        }.bind(this)
-    );
-    return deferred;
-}*/
-
-listCollections = function(callback){
-    var collectionCreated = false;
-    util.log("List Collections")
-    util.log("Access Token: " + this.accessToken);
-    request.get(
-        {
-            'url': this.baseUrl + 'curator/collections',
-            'contentType': 'application/json; charset=utf-8',
-            'headers': {
-                'Authorization': 'Bearer ' + this.accessToken
-            }
-        }, function(err, response, body){
-            if(err){
-                return;
-            }
-            //if(response.status.status ==='OK')
-            util.log("Body: " + body);
-                return;
-        }  
-    )
-    /*.then(
-        function(json){
-            if(json.status.status === 'OK'){
-                for(var i = 0; i < json.collections.length; i++){
-                    var collectionId = json.collections[i].id;
-                    if(collectionId === this.collectionId){
-                        collectionCreated = true;
-                        break;
-                    }
-                }
-            }
-            deferred.resolve(collectionCreated);
-        }.bind(this),
-        function(e){
-            console.error(e);
-            deferred.reject();
-        }.bind(this)
-    );
-    return deferred;*/
+// Turn logging on or off
+log = function(obj){
+    if(this.debug === true){
+        util.log(obj);
+    }
 }
